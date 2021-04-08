85019d252435a52d8594f7b2a428928ee720e8a3
Viv Zara <vzara88@hotmail.com>
2021-04-08 19:41:56 +1000

    Updates to CSS and HTML add of image folder for test purposes

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,17 +2,16 @@ var fetchButton = document.getElementById("fetch-button");
 var repoTable = document.getElementById("something");
 
 function getApi() {
-    var requestUrl = 'https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-added';
+  var requestUrl =
+    "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-added";
 
-    fetch(requestUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            console.log(data)
-
-            
-        });
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 }
 
-fetchButton.addEventListener('click', getApi);
+fetchButton.addEventListener("click", getApi);
